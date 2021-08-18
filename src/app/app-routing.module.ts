b9dab78dd53bd4b3f5fe28bb15b27578f087ff07
Youssef Higazy <yoyohigazy77@gmail.com>
2021-08-18 07:10:10 -0500

    Updating the app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
+import { ForgetPasswordFormComponent } from './forget-password-form/forget-password-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomePageComponent },
-  { path: "auth", component: AuthenticationPageComponent }
+  { path: "auth", component: AuthenticationPageComponent },
+  { path: "forget-password", component: ForgetPasswordFormComponent },
 ];
 
 @NgModule({
